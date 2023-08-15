7239802f7e9f45caab1f25fe33f106a6e4700fcc
wendy <mizukilee@gmail.com>
2023-08-14 23:06:03 -0400

    review and items controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,50 @@
 class ReviewsController < ApplicationController
+    class ItemsController < ApplicationController
+        rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    
+        def index 
+            reviews = Review.all
+            render json: reviews 
+        end
+    
+        def create 
+            review = Review.create(review_params)
+            render json: reivew, status: created 
+        end
+    
+        def show 
+            review = find_review 
+            render json: review
+        end
+    
+        def update 
+            review = find_review 
+            review.update(review_params)
+            render json: review
+        end
+    
+        def destroy 
+            review = find_review
+            review.destroy 
+            head :no_content 
+        end
+    
+    
+        private 
+    
+        def find_review 
+            Review.find(params[:id])
+        end
+    
+        def review_params
+            params.permit(:rating, :comment, :item_id, :customer_id )
+        end
+    
+        def render_not_found_response
+            render json: { error: "Item not found" }, status: :not_found
+        end
+        
+    
+    end 
+
 end
