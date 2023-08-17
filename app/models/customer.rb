76ba44eb0e1f7db1f5ad8fa56531a01a99d3b6b4
wendy <mizukilee@gmail.com>
2023-08-16 21:51:40 -0400

    model assocations done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,8 @@
 class Customer < ApplicationRecord
     has_secure_password
+
+    has_many :reviews
+    has_one :cart
+    has_many :orders
+    has_many :order_items, through: :orders
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,4 @@
 class Review < ApplicationRecord
+    belongs_to :item
+    belongs_to :customer
 end
