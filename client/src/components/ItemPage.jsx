e46f1951e41e7cd5b15b7803f1fea0a262996c9f
wendy <mizukilee@gmail.com>
2023-08-19 18:07:43 -0400

    Itempage1

---
+++ client/src/components/ItemPage.jsx
@@ -0,0 +1,12 @@
+import ItemList from "./ItemList.jsx"
+
+
+export default function ItemPage() { 
+
+
+    return (
+        <div className='itemlist'>
+            <ItemList />
+        </div>
+    )
+}
