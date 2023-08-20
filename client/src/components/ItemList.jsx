1d132abf24a758f4f2159e74802c088235adcf8c
wendy <mizukilee@gmail.com>
2023-08-20 13:45:26 -0400

    padding between navbar and cards

--- client/src/components/ItemList.jsx
+++ client/src/components/ItemList.jsx
@@ -1,13 +1,14 @@
 // single card 
 import ItemCard from "./ItemCard.jsx"
 import Grid from '@mui/material/Grid';
+import "./ItemList.css"
 
 
 export default function ItemList({items}) { 
 
 
     return (
-        <div className='itemcard' style={{ padding: '16px' }}>
+        <div className='itemcard'>
             <Grid container spacing={3}>
                 {items.map(item => (
                     <Grid item xs={12} sm={6} md={4} key={item.key}>
