89f03d2c1ec6cfbe1390eb8879400b145df7494a
wendy <mizukilee@gmail.com>
2023-08-22 20:16:06 -0400

    fix carousel focus issue

--- client/src/components/ImageCarousel.jsx
+++ client/src/components/ImageCarousel.jsx
@@ -4,13 +4,15 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import carous
 
 const ImageCarousel = ({ images }) => {
   return (
-    <Carousel style={{ width: '400px', height: '300px' }}>
+    <div style={{ width: '1000px', height: '600px' }} >
+    <Carousel >
       {images.map((image, index) => (
         <div key={index}>
           <img src={image} alt={`Image ${index}`} style={{ width: '60%', height: '60%', objectFit: 'cover' }} />
         </div>
       ))}
     </Carousel>
+    </div>
   );
 };
 
