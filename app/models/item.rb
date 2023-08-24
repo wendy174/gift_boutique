6bd3a7d6244eeeed9e711606f498948d42b8815b
wendy <mizukilee@gmail.com>
2023-08-23 23:01:46 -0400

    added dep: :destroy to items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
     belongs_to :seller
-    has_many :reviews
+    has_many :reviews, dependent: :destroy
     has_many :cart_items
     has_many :carts, through: :cart_items
     has_many :order_items
