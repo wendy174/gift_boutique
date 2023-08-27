276777161f02e10975af96ac08b90c0f5b0d16ef
wendy <mizukilee@gmail.com>
2023-08-27 15:05:11 -0400

    Carousel size adjusted

--- client/src/components/ImageCarousel.jsx
+++ client/src/components/ImageCarousel.jsx
@@ -4,11 +4,11 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import carous
 
 const ImageCarousel = ({ images }) => {
   return (
-    <div style={{ width: '1000px', height: '600px' }} >
+    <div style={{ width: '600px', height: '300px' }} >
     <Carousel >
       {images.map((image, index) => (
         <div key={index}>
-          <img src={image} alt={`Image ${index}`} style={{ width: '60%', height: '60%', objectFit: 'cover' }} />
+          <img src={image} alt={`Image ${index}`} style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
         </div>
       ))}
     </Carousel>
