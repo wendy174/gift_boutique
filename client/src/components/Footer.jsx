3e567201b43248cb56e8cb1ef466099fefb81912
wendy <mizukilee@gmail.com>
2023-10-25 14:53:58 -0700

    footer material ui

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Grid, Typography, Link, IconButton } from '@material-ui/core';
+import { Box, Grid, Typography, Link, IconButton } from '@mui/material';
 import { Facebook, Twitter, LinkedIn, Instagram } from '@material-ui/icons';
 
 const Footer = () => {
