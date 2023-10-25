33588b6e4a110f4dcb63e76ff7bae79fce72fbfe
wendy <mizukilee@gmail.com>
2023-10-25 12:10:22 -0700

    render attempt

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './components/App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import '@fontsource/inter';
+// import '@fontsource/inter';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
