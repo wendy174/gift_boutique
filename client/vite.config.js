e3fe88793ca10f16fd8f05cfdd28f53e85cfdb50
kirby_son <someuser@duck.com>
2023-10-29 16:06:34 -0700

    changed to docker

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,17 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  
   server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        ws: true,
-        rewrite: path => path.replace(/^\/api/, '')
-      }
-    }
+    port: 4000,
   }
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:3000',
+  //       changeOrigin: true,
+  //       ws: true,
+  //       rewrite: path => path.replace(/^\/api/, '')
+  //     }
+  //   }
+  // }
 
 })
