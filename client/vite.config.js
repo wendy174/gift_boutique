8e6f2f07225168ec09bca9f7190d16739966c734
kirby_son <someuser@duck.com>
2023-10-29 18:59:41 -0700

    updated to load front-end and backend

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,18 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
-    port: 4000,
+    host: true,
+    port: 4000, // This is the port which we will use in docker
+    // Thanks @sergiomoura for the window fix
+    // add the next lines if you're using windows and hot reload doesn't work
+     watch: {
+       usePolling: true
+     }
   }
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://localhost:3000',
-  //       changeOrigin: true,
-  //       ws: true,
-  //       rewrite: path => path.replace(/^\/api/, '')
-  //     }
-  //   }
-  // }
-
-})
+ })
