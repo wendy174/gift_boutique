d000fbeb7029db13a65cdfadea39f2490a5c73c6
wendy <mizukilee@gmail.com>
2023-11-01 13:58:05 -0700

    cors updated

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://giftboutique.wendyprojects.com'  # This is your frontend's origin
+      origins 'http://giftboutique.wendyprojects.com/'  # This is your frontend's origin
       resource '*',
                headers: :any,
                methods: [:get, :post, :put, :patch, :delete, :options, :head],
