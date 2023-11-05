15691d49ffdff72edd244e5dd21800d4655660d3
wendy <mizukilee@gmail.com>
2023-11-05 17:36:36 -0500

    updated skip before action

--- api/app/controllers/items_controller.rb
+++ api/app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-    skip_before_action :authenticate, only: [:index, :show]
+    skip_before_action :authenticate, only: [:index, :show, :destroy, :create]
 
     def index 
         items = Item.all
