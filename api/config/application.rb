aa093ecb828dc7cb420b0e768389eec097d9b539
wendy <mizukilee@gmail.com>
2023-11-06 22:40:10 -0500

    added cors

--- api/config/application.rb
+++ api/config/application.rb
@@ -1,3 +1,65 @@
+# require_relative "boot"
+
+# require "rails"
+# # Pick the frameworks you want:
+# require "active_model/railtie"
+# require "active_job/railtie"
+# require "active_record/railtie"
+# require "active_storage/engine"
+# require "action_controller/railtie"
+# require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+# require "action_view/railtie"
+# require "action_cable/engine"
+# # require "rails/test_unit/railtie"
+
+# # Require the gems listed in Gemfile, including any gems
+# # you've limited to :test, :development, or :production.
+# # Bundler.require(*Rails.groups)
+
+# module GiftBoutique
+#   class Application < Rails::Application
+    
+    
+#     config.middleware.insert_before 0, Rack::Cors do
+#       allow do
+#         origins 'http://3.141.63.83:4000' # Allow requests from your frontend's origin
+#         resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+#       end
+#     end
+    
+    
+    
+    
+#     # Initialize configuration defaults for originally generated Rails version.
+#     config.load_defaults 7.0
+
+#     # Configuration for the application, engines, and railties goes here.
+#     #
+#     # These settings can be overridden in specific environments using the files
+#     # in config/environments, which are processed later.
+#     #
+#     # config.time_zone = "Central Time (US & Canada)"
+#     # config.eager_load_paths << Rails.root.join("extras")
+
+#     # Only loads a smaller set of middleware suitable for API only apps.
+#     # Middleware like session, flash, cookies can be added back manually.
+#     # Skip views, helpers and assets when generating a new resource.
+#     config.api_only = true
+
+#     # ▾ Must add these lines! ▾
+#     # Adding back cookies and session middleware
+#     config.middleware.use ActionDispatch::Cookies
+#     config.middleware.use ActionDispatch::Session::CookieStore
+
+#     # Use SameSite=Strict for all cookies to help protect against CSRF
+#     config.action_dispatch.cookies_same_site_protection = :strict
+
+    
+#   end 
+# end
+
 require_relative "boot"
 
 require "rails"
@@ -16,7 +78,7 @@ require "action_cable/engine"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-# Bundler.require(*Rails.groups)
+Bundler.require(*Rails.groups)
 
 module GiftBoutique
   class Application < Rails::Application
@@ -24,7 +86,7 @@ module GiftBoutique
     
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://3.141.63.83:4000' # Allow requests from your frontend's origin
+        origins 'http://3.141.63.83:4000'# Allow requests from your frontend's origin
         resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
       end
     end
@@ -57,5 +119,5 @@ module GiftBoutique
     config.action_dispatch.cookies_same_site_protection = :strict
 
     
-  end 
+  end
 end
