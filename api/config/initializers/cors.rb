7e2a906ed14ecda15973edb51301998d853d3414
wendy <mizukilee@gmail.com>
2023-11-08 22:58:04 -0500

    updated cors in cors.rb

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -16,13 +16,12 @@
 # end
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      # origins 'http://3.141.63.83'  # removing the :4000 solves cors issue 
-      origins 'http://giftboutique.wendyprojects.com:3000'
-      resource '*',
-               headers: :any,
-               methods: [:get, :post, :put, :patch, :delete, :options, :head],
-               credentials: true
-    end
+  allow do
+    origins 'http://giftboutique.wendyprojects.com', 'http://giftboutique.wendyprojects.com:80'
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :patch, :put, :delete, :options, :head],
+      credentials: true
   end
+end
 
