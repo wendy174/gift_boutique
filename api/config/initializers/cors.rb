2f7116bb22efb2b6a9bb279682e06d33ad1363ef
wendy <mizukilee@gmail.com>
2023-11-08 22:42:45 -0500

    testing: updated to domain name

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -17,7 +17,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://3.141.63.83'  # removing the :4000 solves cors issue 
+      # origins 'http://3.141.63.83'  # removing the :4000 solves cors issue 
+      origins 'giftboutique.wendyprojects.com'
       resource '*',
                headers: :any,
                methods: [:get, :post, :put, :patch, :delete, :options, :head],
